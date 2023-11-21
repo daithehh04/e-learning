4588b26576bed3c92caa123ae2a06bc5705f399c
daithehh04 <daithehh04@gmail.com>
2023-11-21 11:43:42 +0700

    add auth0

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Button, Checkbox, Form, Input } from 'antd';
 const onFinish = (values) => {
-  console.log('Success:', values);
 };
 const onFinishFailed = (errorInfo) => {
-  console.log('Failed:', errorInfo);
 };
 
 function Login() {

002d3baa33bddf8d3d8a821d23f6ec07261dd6e3
daithehh04 <daithehh04@gmail.com>
2023-11-27 00:32:27 +0700

    course detail

--- src/pages/courseDetail/CourseDetail.jsx
+++ src/pages/courseDetail/CourseDetail.jsx
@@ -18,10 +18,10 @@ const course = {
   id: '1a2e',
   category: {
     slug: 'hoc',
-    name: 'toan-1',
+    name: 'Lớp 1',
   },
   slug: 'course-slug',
-  courseName: 'course-name',
+  courseName: 'Toán học 1',
   shortDes: 'good',
   desc: 'helpful',
   avatar:
@@ -33,115 +33,110 @@ const CourseDetail = () => {
   return (
     <>
       <Header />
-
-      <div className={clsx('detail')}>
-        <div className={clsx('wide')}>
-          <div className={clsx('detail__container')}>
-            <div className={clsx('detail__wrapper')}>
-              <div className={clsx('detail__breadcrumb')}>
-                <Breadcrumb separator="›">
+      <div className="wide">
+        <div className={clsx(styles.detailCourse)}>
+          <div className={clsx(styles.detailContainer)}>
+            <Breadcrumb separator="›">
+              <Breadcrumb.Item>
+                <NavLink to={'/'} className={clsx(styles.navLink)}>
+                  Trang chủ
+                </NavLink>
+              </Breadcrumb.Item>
+              {!loading && (
+                <>
                   <Breadcrumb.Item>
                     <NavLink
-                      to={'/'}
-                      className={clsx('detail__breadcumb--link')}
+                      to={`/${course?.category?.slug}`}
+                      className={clsx(styles.navLink)}
                     >
-                      Trang chủ
+                      {course?.category?.name}
                     </NavLink>
                   </Breadcrumb.Item>
-                  {!loading && (
-                    <>
-                      <Breadcrumb.Item>
-                        <NavLink
-                          to={`/${course?.category?.slug}`}
-                          className={clsx('detail__breadcumb--link')}
-                        >
-                          {course?.category?.name}
-                        </NavLink>
-                      </Breadcrumb.Item>
-                      <Breadcrumb.Item>
-                        <NavLink
-                          to={`/${course?.category?.slug}/${course?.slug}`}
-                          className={clsx('detail__breadcumb--link', 'active')}
-                        >
-                          {course?.courseName}
-                        </NavLink>
-                      </Breadcrumb.Item>
-                    </>
-                  )}
-                </Breadcrumb>
+                  <Breadcrumb.Item>
+                    <NavLink
+                      to={`/${course?.category?.slug}/${course?.slug}`}
+                      className={clsx(styles.navLink)}
+                    >
+                      {course?.courseName}
+                    </NavLink>
+                  </Breadcrumb.Item>
+                </>
+              )}
+            </Breadcrumb>
+          </div>
+
+          <Row style={{ width: '100%', margin: '0' }} gutter={16}>
+            <Col xl={16} lg={16} md={24} sm={24} xs={24}>
+              <h1>{course?.courseName}</h1>
+
+              <div>
+                <p>{course?.shortDes}</p>
               </div>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: course?.des ?? '',
+                }}
+              ></div>
+            </Col>
 
-              <Row
-                style={{ width: '100%', margin: '0' }}
-                gutter={16}
-                className={clsx('detail__row')}
-              >
-                <Col xl={16} lg={16} md={24} sm={24} xs={24}>
-                  <h1 className={clsx('detail__name')}>{course?.courseName}</h1>
+            <Col xl={8} lg={8} md={24} sm={24} xs={24}>
+              <div className={styles.infoCourse}>
+                <div className={styles.thumb}>
+                  <img src={course?.avatar || ''} alt="course-avatar" />
+                </div>
+                <h5>Miễn phí</h5>
+                <div className={styles.btnGroup}>
+                  <Row>
+                    <Col
+                      xl={24}
+                      lg={24}
+                      md={12}
+                      sm={24}
+                      xs={24}
+                      className={clsx(styles.btn)}
+                    >
+                      <NavLink
+                        to={`chuong-trinh-hoc/${course?.id}-1`}
+                        className={clsx(styles.btn1)}
+                      >
+                        <button>Chương trình học</button>
+                      </NavLink>
+                    </Col>
 
-                  <div className={clsx('detail__des')}>
-                    <p>{course?.shortDes}</p>
-                  </div>
-                  <div
-                    dangerouslySetInnerHTML={{
-                      __html: course?.des ?? '',
-                    }}
-                    className={clsx('detail__longDes')}
-                  ></div>
-                </Col>
-
-                <Col xl={8} lg={8} md={24} sm={24} xs={24}>
-                  <div className={clsx('detail__badge')}>
-                    <div className={clsx('detail__image')}>
-                      <img
-                        src={course?.avatar || ''}
-                        alt="course-avatar"
-                        className={clsx('detail__avatar')}
-                      />
-                    </div>
-                    <h5 className={clsx('detail__price')}>Miễn phí</h5>
-                    <div className={clsx('detail__group--btn')}>
-                      <Row>
-                        <Col xl={24} lg={24} md={12} sm={24} xs={24}>
-                          <NavLink
-                            to={`chuong-trinh-hoc/${course?.id}-1`}
-                            className={clsx('detail__btn--link')}
-                          >
-                            <button className={clsx('detail__button', 'btn1')}>
-                              Chương trình học
-                            </button>
-                          </NavLink>
-                        </Col>
-
-                        <Col xl={24} lg={24} md={12} sm={24} xs={24}>
-                          <NavLink
-                            to={`de-kiem-tra/${course?.id}-2`}
-                            className={clsx('detail__btn--link')}
-                          >
-                            <button className={clsx('detail__button', 'btn2')}>
-                              Đề kiểm tra
-                            </button>
-                          </NavLink>
-                        </Col>
-                      </Row>
-                    </div>
-                    <ul className={clsx('detail__list')}>
-                      <li className={clsx('detail__item')}>
-                        <FaLightbulb className={clsx('detail__icon')} />
-                        <span>Trình độ cơ bản</span>
-                      </li>
-                      <li className={clsx('detail__item')}>
-                        <FaFilm className={clsx('detail__icon')} />
-                        <span>
-                          Tổng số <strong>{6}</strong> bài học
-                        </span>
-                      </li>
-                      <li className={clsx('detail__item')}>
-                        <FaClock className={clsx('detail__icon')} />
-                        <span>
-                          Thời lượng{' '}
-                          <strong>
-                            {/* {moment(
+                    <Col
+                      xl={24}
+                      lg={24}
+                      md={12}
+                      sm={24}
+                      xs={24}
+                      className={clsx(styles.btn)}
+                    >
+                      <NavLink
+                        to={`de-kiem-tra/${course?.id}-2`}
+                        className={clsx(styles.btn2)}
+                      >
+                        <button>Đề kiểm tra</button>
+                      </NavLink>
+                    </Col>
+                  </Row>
+                </div>
+                <ul className={clsx(styles.detailList)}>
+                  <li className={clsx(styles.detailItem)}>
+                    <FaLightbulb className={clsx(styles.detailIcon)} />
+                    <span>Trình độ cơ bản</span>
+                  </li>
+                  <li className={clsx(styles.detailItem)}>
+                    <FaFilm className={clsx(styles.detailIcon)} />
+                    <span>
+                      Tổng số <strong>{6}</strong> bài học
+                    </span>
+                  </li>
+                  <li className={clsx(styles.detailItem)}>
+                    <FaClock className={clsx(styles.detailIcon)} />
+                    <span>
+                      Thời lượng{' '}
+                      <strong>
+                        {/* {moment(
                               topics
                                 .map((topic, i) =>
                                   topic?.topicChildData.reduce(
@@ -157,28 +152,25 @@ const CourseDetail = () => {
                                   0
                                 ) * 1000
                             ).format("mm:ss")} */}
-                          </strong>
-                        </span>
-                      </li>
-                      <li className={clsx('detail__item')}>
-                        <FaPencilRuler className={clsx('detail__icon')} />
-                        <span>
-                          <strong>{totalExam}</strong> Đề kiểm tra
-                        </span>
-                      </li>
-                      <li className={clsx('detail__item')}>
-                        <FaBatteryFull className={clsx('detail__icon')} />
-                        <span>Học mọi lúc, thi mọi nơi</span>
-                      </li>
-                    </ul>
-                  </div>
-                </Col>
-              </Row>
-            </div>
-          </div>
+                      </strong>
+                    </span>
+                  </li>
+                  <li className={clsx(styles.detailItem)}>
+                    <FaPencilRuler className={clsx(styles.detailIcon)} />
+                    <span>
+                      <strong>{totalExam}</strong> Đề kiểm tra
+                    </span>
+                  </li>
+                  <li className={clsx(styles.detailItem)}>
+                    <FaBatteryFull className={clsx(styles.detailIcon)} />
+                    <span>Học mọi lúc, thi mọi nơi</span>
+                  </li>
+                </ul>
+              </div>
+            </Col>
+          </Row>
         </div>
       </div>
-
       <Footer />
     </>
   );

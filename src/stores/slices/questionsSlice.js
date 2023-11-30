fd5a6714ce32ad72d26904a08798b807d32cc4a9
daithehh04 <daithehh04@gmail.com>
2023-11-30 11:01:38 +0700

    page/learning

--- src/stores/slices/questionsSlice.js
+++ src/stores/slices/questionsSlice.js
@@ -1,45 +1,42 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { requestLoadQuestionsByIdTopic } from '../middleware/questionsMiddeware'
+import { requestLoadQuestionsByIdTopic } from '../middleware/questionsMiddeware';
 const initialState = {
-     loading: false,
-     error: "",
-     questions: [],
-     total: 0,
+  loading: false,
+  error: '',
+  questions: [],
+  total: 0,
 };
 
 export const questionSlice = createSlice({
-     name: "question",
-     initialState,
-     reducers: {},
-     extraReducers: (builder) => {
-          const actionList = [requestLoadQuestionsByIdTopic];
-          actionList.forEach((action) => {
-               builder.addCase(action.pending, (state) => {
-                    state.loading = true;
-               });
-          });
-          actionList.forEach((action) => {
-               builder.addCase(action.rejected, (state) => {
-                    state.loading = false;
-               });
-          });
+  name: 'question',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    const actionList = [requestLoadQuestionsByIdTopic];
+    actionList.forEach((action) => {
+      builder.addCase(action.pending, (state) => {
+        state.loading = true;
+      });
+    });
+    actionList.forEach((action) => {
+      builder.addCase(action.rejected, (state) => {
+        state.loading = false;
+      });
+    });
 
-          // load questions by id topic
-          builder.addCase(
-               requestLoadQuestionsByIdTopic.fulfilled,
-               (
-                    state,
-                    action
-               ) => {
-                    state.questions = action.payload.data;
-                    state.total = action.payload.total;
-                    state.loading = false;
-               }
-          );
-     },
+    // load questions by id topic
+    builder.addCase(
+      requestLoadQuestionsByIdTopic.fulfilled,
+      (state, action) => {
+        state.questions = action.payload.data;
+        state.total = action.payload.total;
+        state.loading = false;
+      }
+    );
+  },
 });
 
-export const { } = questionSlice.actions;
+export const {} = questionSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
 export const QuestionState = (state) => state.question;

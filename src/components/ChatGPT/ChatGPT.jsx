485cb179e92a68127c2d2f748140a4334517e540
xuananh2212 <xuantuananh2212@gmail.com>
2023-12-04 03:50:52 +0700

    add apikey

--- src/components/ChatGPT/ChatGPT.jsx
+++ src/components/ChatGPT/ChatGPT.jsx
@@ -7,7 +7,7 @@ import { GrLinkPrevious } from "react-icons/gr";
 import { useSelector, useDispatch } from 'react-redux';
 import { chatgptSlice } from "../../stores/slices/chatgptSlice";
 const apiKey = `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`;
-const apiKey1 = `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`;
+const apiKey1 = `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`;
 export default function ChatGPT() {
      const dispatch = useDispatch();
      const { toggle } = chatgptSlice.actions;
@@ -15,33 +15,35 @@ export default function ChatGPT() {
      const [arrayUser, setArrayUser] = useState([]);
      const [value, setValue] = useState("");
      async function generateCompletion(value) {
-          console.log(`Bearer ${apiKey}`);
-          const response = await fetch(`https://api.openai.com/v1/chat/completions`, {
-               method: 'POST',
-               headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${apiKey}`
+          if (value) {
+               const response = await fetch(`https://api.openai.com/v1/chat/completions`, {
+                    method: 'POST',
+                    headers: {
+                         "Content-Type": "application/json",
+                         "Authorization": `Bearer ${apiKey1}`
 
-               },
-               body: JSON.stringify({
-                    "model": "gpt-3.5-turbo",
-                    "messages": [
-                         {
-                              "role": "system",
-                              "content": value
-                         },
-                         {
-                              "role": "user",
-                              "content": value
-                         }
-                    ],
-                    "max_tokens": 100
+                    },
+                    body: JSON.stringify({
+                         "model": "gpt-3.5-turbo",
+                         "messages": [
+                              {
+                                   "role": "system",
+                                   "content": value
+                              },
+                              {
+                                   "role": "user",
+                                   "content": value
+                              }
+                         ],
+                         "max_tokens": 200
 
 
+                    })
                })
-          })
-          const data = await response.json();
-          return data;
+               const data = await response.json();
+               return data;
+          }
+          return null;
      }
      const handleSubmit = (e) => {
           e.preventDefault();
